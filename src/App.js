a5ac4ff04df18c7004efc2f0ce9065295302769d
Qudusayo <qqudusayo@gmail.com>
2022-06-15 01:01:46 +0100

    Patch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { UAuthMoralisConnector } from "@uauth/moralis";
+import { useEffect } from "react";
 import { useMoralis } from "react-moralis";
 import { uauth } from "./connectors";
 
 function App() {
   const { isAuthenticated, logout, authenticate } = useMoralis();
+  const uauthMoralisConnector = new UAuthMoralisConnector();
 
   const userLogIn = async () => {
     try {
       let user = await authenticate(uauth);
+      let domainDeatils = uauthMoralisConnector.uauth.user()
+        
+      console.log("DOMAIN NAME: " + (await domainDeatils).sub)
+      console.log("OWNER: " + (await domainDeatils).wallet_address)
+
       console.log(user);
     } catch (error) {
       console.log(error);
